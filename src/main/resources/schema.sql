c325cd82a7438405498dacc67cb2732a1047b1c3
Avadhut <mali.avadhut7@gmail.com>
2025-06-08 03:34:00 +0530

    implemented the course and student controllers

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,19 @@
-CREATE TABLE Students(
+CREATE TABLE IF NOT EXISTS Students (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR2(255) NOT NULL,
-    email VARCHAR2(255) UNIQUE NOT NULL
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE Courses(
+CREATE TABLE IF NOT EXISTS Courses (
     courseId INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR2(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
     description TEXT
 );
 
-CREATE TABLE Enrollment(
+CREATE TABLE IF NOT EXISTS Enrollment (
     student_id INT,
     course_id INT,
-    PRIMARY KEY(student_id,course_id),
-    FOREIGN KEY(student_id) REFERENCES Students(student_id) ON DELETE CASCADE,
-    FOREIGN KEY(course_id) REFERENCES Courses(course_id) ON DELETE CASCADE
+    PRIMARY KEY (student_id, course_id),
+    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES Students(id) ON DELETE CASCADE,
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES Courses(courseId) ON DELETE CASCADE
 );
